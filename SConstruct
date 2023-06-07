778c299786e5312d69d5e12968251a8bdc57a4db
samfromcadott <samahstan@gmail.com>
2023-06-07 18:23:45 -0500

    Moved methods out of class

--- SConstruct
+++ SConstruct
@@ -1,7 +1,10 @@
 import os
 
 VariantDir('build', 'tests', duplicate=False)
-env = Environment( CPPPATH=['./', './doctest/doctest'] )
+env = Environment(
+	CPPPATH=['./', './doctest/doctest'],
+	CXXFLAGS='-O3'
+)
 env['ENV']['TERM'] = os.environ['TERM'] # Colored output
 
 env.Program("test", ["build/main.cc"])
--- real.hh
+++ real.hh
@@ -10,101 +10,129 @@ private:
 
 public:
 	// Arithmetic operators
-	inline real operator+(real other) {
-		real result;
-		result.value = value + other.value;
-		return result;
-	}
-
-	inline real operator-(real other) {
-		real result;
-		result.value = value - other.value;
-		return result;
-	}
-
-	inline real operator*(real other) {
-		real result;
-		result.value = (value * other.value) >> shift;
-		return result;
-	}
-
-	inline real operator/(real other) {
-		real result;
-		result.value = (value << shift) / other.value;
-		return result;
-	}
+	real operator+(real other);
+	real operator-(real other);
+	real operator*(real other);
+	real operator/(real other);
 
 	// Assignment operators
-	inline real& operator=(const real& other) {
-		value = other.value;
-		return *this;
-	}
-
-	inline real& operator=(const int& other) {
-		*this = real(other);
-		return *this;
-	}
-
-	inline real& operator=(const int64_t& other) {
-		*this = real(other);
-		return *this;
-	}
-
-	inline real& operator=(const float& other) {
-		*this = real(other);
-		return *this;
-	}
-
-	inline real& operator=(const double& other) {
-		*this = real(other);
-		return *this;
-	}
+	real& operator=(const real& other);
+	real& operator=(const int& other);
+	real& operator=(const int64_t& other);
+	real& operator=(const float& other);
+	real& operator=(const double& other);
 
 	// Comparison operators
-	inline bool operator==(const real& other) {
-		return value == other.value;
-	}
+	bool operator==(const real& other);
 
 	// Type conversions
-	inline operator long() const {
-		int64_t n = value;
-		n >>= shift;
-		n |= (unsigned long)(value<0) << 63; // Apply the sign
-		return n;
-	}
-
-	inline operator float() const {
-		return float(value) / float(ONE);
-	}
-
-	inline operator double() const {
-		return double(value) / double(ONE);
-	}
+	operator long() const;
+	operator float() const;
+	operator double() const;
 
 	// Constructors
-	inline real() {
-		value = 0;
-	}
-
-	inline real(const int64_t value) {
-		int64_t n = value;
-		this->value = n << shift;
-		this->value |= (int64_t)(value<0) << 63; // Apply the original sign
-	}
-
-	inline real(const int32_t value) {
-		int64_t n = value;
-		this->value = n << shift;
-		this->value |= (int64_t)(value<0) << 63; // Apply the original sign
-	}
-
-	inline real(const double value) {
-		this->value = int64_t(value * ONE);
-	}
-
-	inline real(const float value) {
-		this->value = int64_t(value * ONE);
-	}
+	real();
+	real(const int64_t value);
+	real(const int32_t value);
+	real(const double value);
+	real(const float value);
 
 	virtual ~real(){}
 };
+
+// Arithmetic operators
+real real::operator+(real other) {
+	real result;
+	result.value = value + other.value;
+	return result;
+}
+
+real real::operator-(real other) {
+	real result;
+	result.value = value - other.value;
+	return result;
+}
+
+real real::operator*(real other) {
+	real result;
+	result.value = (value * other.value) >> shift;
+	return result;
+}
+
+real real::operator/(real other) {
+	real result;
+	result.value = (value << shift) / other.value;
+	return result;
+}
+
+// Assignment operators
+real& real::operator=(const real& other) {
+	value = other.value;
+	return *this;
+}
+
+real& real::operator=(const int& other) {
+	*this = real(other);
+	return *this;
+}
+
+real& real::operator=(const int64_t& other) {
+	*this = real(other);
+	return *this;
+}
+
+real& real::operator=(const float& other) {
+	*this = real(other);
+	return *this;
+}
+
+real& real::operator=(const double& other) {
+	*this = real(other);
+	return *this;
+}
+
+// Comparison operators
+bool real::operator==(const real& other) {
+	return value == other.value;
+}
+
+// Type conversions
+real::operator long() const {
+	int64_t n = value;
+	n >>= shift;
+	n |= (unsigned long)(value<0) << 63; // Apply the sign
+	return n;
+}
+
+real::operator float() const {
+	return float(value) / float(ONE);
+}
+
+real::operator double() const {
+	return double(value) / double(ONE);
+}
+
+// Constructors
+real::real() {
+	value = 0;
+}
+
+real::real(const int64_t value) {
+	int64_t n = value;
+	this->value = n << shift;
+	this->value |= (int64_t)(value<0) << 63; // Apply the original sign
+}
+
+real::real(const int32_t value) {
+	int64_t n = value;
+	this->value = n << shift;
+	this->value |= (int64_t)(value<0) << 63; // Apply the original sign
+}
+
+real::real(const double value) {
+	this->value = int64_t(value * ONE);
+}
+
+real::real(const float value) {
+	this->value = int64_t(value * ONE);
+}
